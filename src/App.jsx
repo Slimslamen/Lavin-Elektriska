d076d40320841bf6b21c9b471e379758d6fbcf8e
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2025-09-13 21:41:47 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Main from "./pages/MainComponent";
 import Policy from "./pages/policyPage";
-import NotFound from "./pages/notFound";
+import NotFound from "./pages/notFound.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BlogProvider } from "./Context/BlogProvider"
 import './App.css'
